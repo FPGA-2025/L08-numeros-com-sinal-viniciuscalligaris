60b225ae316f1053e544109ecad74f9a9e46b4cf
Vinícius César Bertolo Calligaris <vinicius.cesar7@hotmail.com>
2025-04-17 15:23:22 -0300

    Finalizado

--- numeros_com_sinal.v
+++ numeros_com_sinal.v
@@ -7,6 +7,14 @@ module numeros_com_sinal(
     output reg [7:0] saida
 );
 
-//insira seu codigo aqui
+    always @(*) begin
+        case (codigo)
+            2'b00: saida = entrada_signed_1 + entrada_signed_2;
+            2'b01: saida = entrada_unsigned_1 + entrada_unsigned_2;
+            2'b10: saida = entrada_signed_1 + entrada_unsigned_1;
+            2'b11: saida = entrada_signed_2 + entrada_unsigned_1;
+            default: saida = 8'b0; 
+        endcase
+    end
 
 endmodule
